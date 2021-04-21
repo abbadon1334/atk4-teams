a590a9a86d8feff7d069242b8a2e6fcf965aeb71
Francesco Danti <fdanti@gmail.com>
2021-04-21 11:05:35 +0200

    Fix : chrome80 + crossorigin

--- src/Teams.php
+++ src/Teams.php
@@ -5,132 +5,28 @@ declare(strict_types=1);
 namespace Atk4\Teams;
 
 use Atk4\Container\AppContainer;
-use Atk4\Core\AppScopeTrait;
 use Atk4\Core\Exception;
-use Atk4\Core\NameTrait;
 use Atk4\Core\SessionTrait;
 use Atk4\Data\Persistence\Array_;
 use Atk4\Teams\Data\UserTeams;
-use Delight\Cookie\Cookie;
+use Atk4\Ui\AbstractView;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-use League\OAuth2\Client\Token\AccessToken;
+use TheNetworg\OAuth2\Client\Token\AccessToken;
 use TheNetworg\OAuth2\Client\Provider\Azure;
 use Throwable;
 
-class Teams
+class Teams extends AbstractView
 {
-    use AppScopeTrait;
-    use NameTrait;
     use SessionTrait;
 
     private Azure        $provider;
-    private ?AccessToken $token;
+    private ?AccessToken $accessToken;
     private AppContainer $container;
     private UserTeams    $userTeams;
 
     public function __construct(AppContainer $container)
     {
         $this->container = $container;
-
-        $this->setProvider();
-
-        $this->token = $this->getToken();
-        $this->setupUserTeamsModel();
-    }
-
-    private function getToken(): ?AccessToken
-    {
-        $serialized = $this->recall('teams_token');
-
-        return $serialized === null
-            ? null
-            : unserialize($serialized);
-    }
-
-    private function setupUserTeamsModel(): void
-    {
-        $persistence = new Array_([]);
-        $this->userTeams = new UserTeams($persistence);
-        $this->userTeams->data = unserialize($this->recall('teams_user', "a:0:{}"));
-        $this->userTeams->setId($this->userTeams->data['id'] ?? null);
-    }
-
-    public function authenticate()
-    {
-
-        $this->getApp()->setResponseHeader('Access-Control-Allow-Origin', "*");
-        $this->getApp()->setResponseHeader('Access-Control-Allow-Headers', "*");
-
-        // cookie is created only during requestAuth()
-        // if cookie exists we must call the callback()
-        // in the callback the cookie will be deleted
-        if (Cookie::get('TEAM_AUTH_STATE', '') !== '') {
-            $this->callback(); // client will be redirect
-        }
-
-        // We have a token
-        if ($this->token !== null) {
-            // check if is expired and refresh if not
-            if ($this->token->hasExpired()) {
-                if ($this->token->getRefreshToken() !== null) {
-                    $this->refreshToken();
-                } else {
-                    $this->forgetToken();
-                    $this->authenticate(); // client will be redirect
-                }
-            }
-        }
-
-        // we don't have a token
-        if ($this->token === null) {
-            $this->requestAuth(); // client will be redirect
-        }
-
-        // client authentication : OK
-
-        // call Graph api /me and refresh user
-        $this->refreshWhoAMI();
-    }
-
-    public function callback()
-    {
-        try {
-            if (!Cookie::exists('TEAM_AUTH_STATE')) {
-                throw new Exception('Something is wrong, cookie not exists');
-            }
-
-            $cookie_teams = (string)Cookie::get('TEAM_AUTH_STATE');
-
-            // we already set the cookie with a really short expire
-            // we try to delete it
-            (new Cookie('TEAM_AUTH_STATE'))->deleteAndUnset();
-
-            $teams_code = $_GET['code'] ?? null;
-            $teams_state = $_GET['state'] ?? null;
-
-            if ($teams_code && $cookie_teams && $teams_state) {
-                if ($teams_state === $cookie_teams) {
-                    // @var AccessToken $token
-                    $this->token = $this->provider->getAccessToken('authorization_code', [
-                        'scope' => $this->provider->scope,
-                        'code'  => $teams_code,
-                    ]);
-
-                    $this->serializeToken($this->token);
-                    $this->redirect($this->container->get('teams/app_redirect_uri_on_success'));
-                }
-            }
-        } catch (Throwable $e) {
-            if ($this->getApp() === null) {
-                echo $e->getMessage();
-            }
-
-            throw new Exception($e->getMessage());
-        }
-    }
-
-    private function setProvider()
-    {
         $this->provider = new Azure([
             'clientId'               => $this->container->get('teams/app_id'),
             'clientSecret'           => $this->container->get('teams/app_secret'),
@@ -138,6 +34,107 @@ class Teams
             'scopes'                 => $this->container->get('teams/app_scopes'),
             'defaultEndPointVersion' => Azure::ENDPOINT_VERSION_2_0,
         ]);
+
+        // Fix : chrome80 + cross domain session check
+        session_set_cookie_params($this->container->get('teams/session_cookie_params'));
+    }
+
+    /**
+     * @throws Exception
+     * @throws IdentityProviderException
+     */
+    protected function init(): void
+    {
+        parent::init();
+
+        $this->initUserTeams();
+        $this->initAccessToken();
+
+        $this->getApp()->setResponseHeader('Access-Control-Allow-Credentials', 'true');
+        $this->getApp()->setResponseHeader('Access-Control-Allow-Origin', "*");
+        $this->getApp()->setResponseHeader('Access-Control-Allow-Headers', "*");
+
+        // We have a token
+        if ($this->accessToken !== null) {
+
+            if (!$this->accessToken->hasExpired()) {
+                $this->refreshWhoAMI();
+
+                return;
+            }
+
+            if ($this->accessToken->getRefreshToken() !== null) {
+                $this->refreshToken();
+            } else {
+                $this->forgetToken();
+            }
+
+            return;
+        }
+
+        $this->checkAuth(); // client will be redirect
+    }
+
+    private function initAccessToken()
+    {
+        $serialized = $this->recall('teams_token');
+
+        $this->accessToken = $serialized === null
+            ? null
+            : unserialize($serialized);
+    }
+
+    private function initUserTeams()
+    {
+        $this->userTeams = new UserTeams(new Array_());
+        $this->userTeams->data = unserialize($this->recall('teams_user', "a:0:{}"));
+        $this->userTeams->setId($this->userTeams->data['id'] ?? null);
+    }
+
+    /**
+     * @throws Exception|IdentityProviderException
+     */
+    public function checkAuth()
+    {
+
+        $teams_code = $_GET['code'] ?? null;
+        if (null === $teams_code) {
+            $this->requestAuth();
+        } else {
+            $this->callback();
+        }
+    }
+
+    /**
+     * @throws \Atk4\Ui\Exception
+     * @throws IdentityProviderException
+     */
+    public function callback()
+    {
+        $teams_code = $_GET['code'] ?? null;
+        $teams_state = $_GET['state'] ?? null;
+
+        $saved_state = $this->recall('session_state');
+
+        if($saved_state === null) {
+            throw new \Atk4\Ui\Exception("Something wrong, initial state not found");
+        }
+
+        if($saved_state !== $teams_state) {
+            throw new \Atk4\Ui\Exception("Something wrong, initial state not matching with returned state");
+        }
+
+        $this->forget('session_state');
+
+        /** @var AccessToken $accessToken */
+        $accessToken = $this->provider->getAccessToken('authorization_code', [
+            'scope' => $this->provider->scope,
+            'code'  => $teams_code,
+        ]);
+
+        $this->serializeToken($accessToken);
+
+        $this->redirect($this->container->get('teams/app_redirect_uri_on_success'));
     }
 
     private function serializeToken(AccessToken $token): void
@@ -147,12 +144,6 @@ class Teams
 
     private function redirect(string $uri)
     {
-        if ($this->getApp() === null) {
-            header('Location: ' . $uri);
-
-            exit;
-        }
-
         $this->getApp()->redirect($uri);
     }
 
@@ -160,30 +151,33 @@ class Teams
     {
         $authorizationUrl = $this->provider->getAuthorizationUrl(['scope' => $this->provider->scope]);
 
-        $cookie = new Cookie('TEAM_AUTH_STATE');
-        $cookie->setValue($this->provider->getState());
-        $cookie->setMaxAge(5);
-        $cookie->setSecureOnly(true);
-        $cookie->setSameSiteRestriction('None');
-        $cookie->saveAndSet();
+        $this->memorize('session_state', $this->provider->getState());
 
         $this->redirect($authorizationUrl);
     }
 
+    /**
+     * @throws \Atk4\Data\Exception
+     */
     public function refreshWhoAMI(bool $force = false): void
     {
         if ($this->userTeams->loaded() && !$force) {
             return;
         }
 
+        /** @var AccessToken $token */
+        $token = $this->accessToken;
         $data = $this->provider->get(
-            $this->provider->getRootMicrosoftGraphUri($this->token) . '/v1.0/me',
-            $this->token
+            $this->provider->getRootMicrosoftGraphUri($token) . '/v1.0/me',
+            $this->accessToken
         );
 
         $this->setTeamUser($data);
     }
 
+    /**
+     * @throws \Atk4\Data\Exception
+     */
     private function setTeamUser(array $data)
     {
         unset($data['@odata.context']); // remove useless data
@@ -204,10 +198,11 @@ class Teams
     {
         $this->forget('teams_user');
         $this->forget('teams_token');
-        $this->token = null;
+        $this->forget('session_state');
+        $this->accessToken = null;
     }
 
-    private function logout()
+    public function logout()
     {
         $this->forgetToken();
         $this->redirect($this->container->get('teams/app_redirect_uri_on_success'));
@@ -216,12 +211,13 @@ class Teams
     private function refreshToken()
     {
         try {
-            $this->token = $this->provider->getAccessToken('refresh_token', [
+            /** @var AccessToken $accessToken */
+            $accessToken = $this->provider->getAccessToken('refresh_token', [
                 'scope'         => $this->provider->scope,
-                'refresh_token' => $this->token->getRefreshToken(),
+                'refresh_token' => $this->accessToken->getRefreshToken(),
             ]);
-
-            $this->serializeToken($this->token);
+            $this->accessToken = $accessToken;
+            $this->serializeToken($this->accessToken);
         } catch (IdentityProviderException $e) {
             $this->forgetToken();
         }
